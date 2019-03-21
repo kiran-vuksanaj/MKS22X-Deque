d76137d0ecdbbf733c4518089a8fe0e3f9fcbef0
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-21 19:30:28 -0400

    note to self of full length driver necessary capacity

---
+++ Driver.java
@@ -0,0 +1,17 @@
+public class Driver{
+  public static void main(String[] args){
+    /*THINGS TO TEST
+    1. constructor, default
+    2. constructor, varying initialCapacity
+    3. constructor, 0 initialCapacity
+    4. specific series of adds/removes and toString
+    4. randomized addFirst,addLast,removeFirst,removeLast,getFirst,getLast, with size
+    5. getFirst/getLast/remove return values immediately following add
+    6. get/remove returned values 'digging' from opposite side
+    7. nullpointer exception verification
+    8. NSEE exception verification
+    9. massive series of add/removes, make sure data looping is used
+    10. forced resize and subsequent object removal
+    */
+  }
+}
