c6dc47c0a45c61070728ac8aadb9183857b3612e
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-21 00:51:40 -0400

    more fields, constructor

--- MyDeque.java
+++ MyDeque.java
@@ -1,4 +1,10 @@
 public class MyDeque<E>{
   private E[] data;
-  private int size;
+  private int size,s,e;
+  public MyDeque(){
+    data = new E[10];
+    size = 0;
+    s = 0;
+    e = 0;
+  }
 }
