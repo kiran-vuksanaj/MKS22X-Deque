e746af7531d6d3333e326edd7a35c933d65de604
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-21 09:44:59 -0400

    capacity variable constructor

--- MyDeque.java
+++ MyDeque.java
@@ -2,8 +2,14 @@ public class MyDeque<E>{
   private E[] data;
   private int size,s,e;
   @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity]; //this is SO WEIRD
+    size = 0;
+    s = 0;
+    e = 0;
+  }
   public MyDeque(){
-    data = (E[])new Object[10]; //this is SO WEIRD
+    data = (E[])new Object[10];
     size = 0;
     s = 0;
     e = 0;
