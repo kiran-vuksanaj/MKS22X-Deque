6c0b5265162eb946c6d0b41387bfa5499088f878
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-21 19:17:29 -0400

    corrections to resize

--- MyDeque.java
+++ MyDeque.java
@@ -22,11 +22,13 @@ public class MyDeque<E>{
   }
 
   public void addFirst(E element){
+    if(data.length <= size()) resize();
     s = correctIndex(s-1);
     data[s] = element;
     size++;
   }
   public void addLast(E element){
+    if(data.length <= size()) resize();
     data[e] = element;
     e = correctIndex(e+1);
     size++;
@@ -60,18 +62,22 @@ public class MyDeque<E>{
     return out + "}";
   }
   private void resize(){
+    //System.out.println(Arrays.toString(data));
     E[] replace = (E[])new Object[data.length*2+1];
     int i = s;
     int newI = 0;
-    while(i != e){
+    while(newI < size()){
       replace[newI++] = data[i];
       i = correctIndex(i+1);
     }
+    data = replace;
+    //System.out.println(Arrays.toString(data));
     s = 0;
     e = size();
   }
   public static void main(String[] args){
     MyDeque<String> m = new MyDeque<String>();
+    /*
     System.out.println(m.correctIndex(-1));
     System.out.println(m.correctIndex(10));
     System.out.println(m.correctIndex(11));
@@ -87,5 +93,16 @@ public class MyDeque<E>{
     System.out.println(m.removeLast());
     System.out.println(m.getLast());
     System.out.println(m+" "+m.size());
+    */
+    for(int i=0;i<20;i++){
+      m.addFirst("j");
+      m.addLast("h");
+    }
+    System.out.println(m);
+    while(m.size()>0){
+      System.out.println(m.removeFirst());
+      System.out.println(m.removeLast());
+    }
+    System.out.println(m);
   }
 }
