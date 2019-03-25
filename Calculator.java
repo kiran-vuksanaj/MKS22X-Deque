83fab421bbcfd1fb7815aa4d71393dadc1ef35de
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-25 10:06:40 -0400

    corrected issues

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,12 @@
 public class Calculator{
   public static void main(String[] args){
-
+    System.out.println(eval(String.join(" ",args)));
   }
   public static double eval(String s){
     String[] tokens = s.split("\\s+");
-    for(String s : tokens){
-      System.out.println(s);
+    for(String token : tokens){
+      System.out.println(token);
     }
+    return -1;
   }
 }
